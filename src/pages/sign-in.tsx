5af58a274867e9c64e5c3dc1a20a9f22d2f5afbc
Carlos Faustino <chfcchfc96@gmail.com>
2026-01-22 18:17:27 -0300

    feat: add link to sign up page

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router"
+
 import { Button } from "@/components/button"
 import { Input } from "@/components/input"
 
@@ -18,7 +20,14 @@ export function SignIn() {
         placeholder="123456"
       />
 
-      <Button>Entrar</Button>
+      <Button type="submit">Entrar</Button>
+
+      <Link
+        to="/sign-up"
+        className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 ease-linear transition"
+      >
+        Criar conta
+      </Link>
     </form>
   )
 }
