6f212276d6145aedde653aa1488f0fc129cbe086
Carlos Faustino <chfcchfc96@gmail.com>
2026-01-26 13:08:47 -0300

    feat: add button variants

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,11 +1,23 @@
+import { classMerge } from "@/utils/class-merge"
+
 interface ButtonProps extends React.ComponentProps<"button"> {
   isLoading?: boolean
+  variant?: keyof typeof variants.button
+}
+
+const variants = {
+  button: {
+    base: "h-12",
+    icon: "size-12",
+    iconSmall: "size-10",
+  },
 }
 
 export function Button({
   isLoading,
   className,
   type = "button",
+  variant = "base",
   children,
   ...props
 }: ButtonProps) {
@@ -13,10 +25,11 @@ export function Button({
     <button
       type={type}
       data-loading={isLoading}
-      className={[
-        "flex items-center justify-center rounded-lg bg-green-100 text-white cursor-pointer not-data-[loading=true]:hover:bg-green-200 transition ease-linear h-12 data-[loading=true]:opacity-50 data-[loading=true]:cursor-progress",
+      className={classMerge(
+        "flex items-center justify-center rounded-lg bg-green-100 text-white cursor-pointer not-data-[loading=true]:hover:bg-green-200 transition ease-linear data-[loading=true]:opacity-50 data-[loading=true]:cursor-progress",
+        variants.button[variant],
         className,
-      ].join(" ")}
+      )}
       {...props}
     >
       {children}
