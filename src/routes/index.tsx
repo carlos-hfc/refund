82959563ce7a68ec4687022b96fbeab38f0ae996
Carlos Faustino <chfcchfc96@gmail.com>
2026-02-05 11:34:33 -0300

    feat: add user session to show routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,16 +2,35 @@ import { BrowserRouter } from "react-router"
 
 import { Loading } from "@/components/loading"
 
+import { AuthRoutes } from "./auth-routes"
+import { EmployeeRoutes } from "./employee-routes"
 import { ManagerRoutes } from "./manager-routes"
 
 const IS_LOADING = false
 
+const session = {
+  user: {
+    role: "manager",
+  },
+}
+
 export function Routes() {
+  function Route() {
+    switch (session?.user.role) {
+      case "employee":
+        return <EmployeeRoutes />
+      case "manager":
+        return <ManagerRoutes />
+      default:
+        return <AuthRoutes />
+    }
+  }
+
   if (IS_LOADING) return <Loading />
 
   return (
     <BrowserRouter>
-      <ManagerRoutes />
+      <Route />
     </BrowserRouter>
   )
 }
